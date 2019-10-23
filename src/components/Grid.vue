286496e109a0d2ea9cc2c203e6171be86b5a6d56
mouafa <mouafa.ahmed@smartfrog.com>
2019-10-23 22:36:38 +0200

    grid comp

---
+++ src/components/Grid.vue
@@ -0,0 +1,44 @@
+<style scoped lang="stylus">
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.row {
+  display: flex;
+}
+
+.column {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  background-color: blue;
+  width: 100px;
+  height: @width;
+  color: white;
+  font-size: @width*0.5
+}
+</style>
+
+<template>
+  <div class="container">
+    <div v-for="row in grid" :key="row" class="row">
+      <div v-for="col in row" :key="col" class="column">{{col}}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Grid",
+  props: {
+    grid: Array
+  }
+});
+</script>
+
+
