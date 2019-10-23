077a080dcd56af79b9a0684e9a92020ad91d8a97
mouafa <mouafa.ahmed@smartfrog.com>
2019-10-23 22:07:40 +0200

    intitial store

---
+++ src/store/grid.ts
@@ -0,0 +1,40 @@
+import { Module, ActionTree, MutationTree } from "vuex";
+
+type ColumnContent = number | undefined;
+type Grid = ColumnContent[][];
+
+interface State {
+  grid: Grid;
+}
+
+interface RootState {}
+
+const state: State = {
+  grid: [[]]
+};
+
+const actions: ActionTree<State, RootState> = {
+  simpleGrid({ commit }) {
+    const newGrid: Grid = [[1, 2, 3], [4, 5, 6]];
+    commit("SET_GRID", newGrid);
+  }
+};
+
+const mutations: MutationTree<State> = {
+  SET_GRID(state, payload) {
+    state.grid = payload;
+    return state;
+  }
+};
+
+export const gridModule: Module<State, RootState> = {
+  namespaced: true,
+  state,
+  actions,
+  mutations,
+  getters: {
+    currentGrid(state) {
+      return state.grid;
+    }
+  }
+};
